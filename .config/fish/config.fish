56e237d333caba4e7b9f9055eb3317e21cf8a2ae
Jeffery Smith <toxicglados@gmail.com>
2024-06-14 15:56:27 -0500

    Add various aliases, PATH elements, and extras that have accrued over time

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,17 @@ function fish_prompt
     set -l namecol  white
     set -l dircol green
     set -l branchcol purple
+    set -l proxycol red
+    set -l timecol blue
     # Print username:
     set_color $namecol -b normal
     echo -n (whoami)":"
+
+    # Print time
+    set_color $timecol -b normal
+    echo -n "["(date +%H:%M:%S)"]"
+    set_color $namecol -b normal
+    echo -n ":"
 
     # Print git_branch_name
     set_color $branchcol -b normal
@@ -27,26 +35,40 @@ end
 # Alias definitions.
 alias back 'prevd'
 alias forth 'nextd'
+alias k 'kubectl'
+alias vim 'nvim'
+alias view 'nview'
+
 
 set PATH /home/jeff/.local/bin $PATH
 set PATH /home/jeff/gems/bin $PATH
+set PATH /home/jeff/go/bin $PATH
+set PATH /home/jeff/.pulumi/bin $PATH
 set -gx GEM_HOME /home/jeff/gems
-set -gx DOCKER_HOST tcp://localhost:2375
 set -gx PIPENV_MAX_DEPTH 10
 set -gx AWS_PROFILE vuemgmt-cdpipeline
+set -gx NVM_DIR /home/jeff/.nvm
+set -gx EDITOR vim
+
+# Add rust stuff to path
+source "$HOME/.cargo/env.fish"
 
 # Fundle plugins
-fundle plugin 'sentriz/fish-pipenv'
-
+#fundle plugin 'sentriz/fish-pipenv'
+fundle plugin 'evanlucas/fish-kubectl-completions'
 
 fundle init
 
 
-# Start x and compton
+
+if status is-interactive
+  atuin init fish --disable-up-arrow | source
+end
+
+# Start X
 if status is-login
     if test -z "$DISPLAY"
         startx
         feh --bg-scale ~/background.jpeg
-        compton &
     end
 end
